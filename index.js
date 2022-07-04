0c0c7a39d7999743605d2cb73c0bc895b0a9f6fb
Giorgio Grassini <giorgiograssini@gmail.com>
2022-07-04 20:37:01 +0200

    moved port in dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express');
+require('dotenv').config()
+const express = require("express");
 const app = express();
 
-app.listen(3000, () => {
-  console.log('server running')
+app.listen(process.env.PORT || 3000, () => {
+  console.log("server running");
 });
