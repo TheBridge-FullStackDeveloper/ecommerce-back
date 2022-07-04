8e47b59e4a49eb964cf29fc9e8627938c15e0717
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-04 20:43:19 +0200

    Port added on console log

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("server running");
+  console.log('server running on PORT 3000');
 });
