53aa39634d7bb8831a0ddef9f1f93319afe32850
lucho532 <98648360+lucho532@users.noreply.github.com>
2022-07-06 19:42:13 +0200

    he creado la carpeta confis con su archivo db he inclui una carpeta .… (#16)
    * he creado la carpeta confis con su archivo db he inclui una carpeta .env para guardar las const
    
    * he añadito variables de entorno al documento yml guardandolas en el archivo .env
    
    * he modificado el console.log del index para q el puerto sea dinamico

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
+const db = require("./configs/db");
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('server running on PORT 3000');
+  console.log(`server running on PORT ${process.env.PORT}`);
 });
