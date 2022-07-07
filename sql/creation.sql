4ebf19c226325b00dd6245932603c6fd8c9767fa
sol <ssuarezf84@gmail.com>
2022-07-07 17:01:12 +0200

    modificacion creacion bbdd

--- sql/creation.sql
+++ sql/creation.sql
@@ -1,27 +1,3 @@
--- borrado tabla "orders"
-DROP TABLE IF EXISTS orders; 
-
--- creacion tabla "orders"; primary key orderID
-CREATE TABLE IF NOT EXISTS orders (
-  orderId SERIAL PRIMARY KEY, 
-  quantity INT NOT NULL,
-  userId INT FOREIGN KEY REFERENCES users (userId), --referencia la tabla "orders" con la clave de la tabla "users"
-  productId INT FOREIGN KEY REFERENCES products (productId), --referencia la tabla "orders" con la clave de la tabla "products"
-);
-
--- borrado tabla "users"
-DROP TABLE IF EXISTS users;
-
--- creacion tabla "users"; primary key userId
-CREATE TABLE IF NOT EXISTS users (
-  userId SERIAL PRIMARY KEY,
-  firstName TEXT NOT NULL,
-  email TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL,
-  address TEXT NOT NULL,
-  role TEXT NOT NULL,
-);
-
 -- borrado tabla "products"
 DROP TABLE IF EXISTS products;
 
@@ -34,5 +10,29 @@ CREATE TABLE IF NOT EXISTS products (
   quantity INT NOT NULL,
   img TEXT NOT NULL,
   details TEXT NOT NULL,
-  rate TEXT NOT NULL,
+  rate TEXT NOT NULL
+);
+
+-- borrado tabla "users"
+DROP TABLE IF EXISTS users;
+
+-- creacion tabla "users"; primary key userId
+CREATE TABLE IF NOT EXISTS users (
+  userId SERIAL PRIMARY KEY,
+  firstName TEXT NOT NULL,
+  email TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  address TEXT NOT NULL,
+  role TEXT NOT NULL
+);
+
+-- borrado tabla "orders"
+DROP TABLE IF EXISTS orders; 
+
+-- creacion tabla "orders"; primary key orderID
+CREATE TABLE IF NOT EXISTS orders (
+  orderId SERIAL PRIMARY KEY, 
+  quantity INT NOT NULL,
+  userId smallint REFERENCES users(userId), --referencia la tabla "orders" con la clave de la tabla "users"
+  productId smallint REFERENCES products(productId) --referencia la tabla "orders" con la clave de la tabla "products"
 );
