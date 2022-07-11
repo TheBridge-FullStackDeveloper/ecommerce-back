c64c5455398922cfc8d7f6923509fc19dee58a25
Brujitari <natimontesg2@mail.com>
2022-07-11 19:47:30 +0200

    auwbfhwhejn

--- index.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const db = require("./configs/db");
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`server running on PORT ${process.env.PORT}`);
-});
