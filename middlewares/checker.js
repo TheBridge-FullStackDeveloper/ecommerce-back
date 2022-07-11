1fb3b79320bac40276096bedb34fd9c14e53514d
jaengustavof <67112627+jaengustavof@users.noreply.github.com>
2022-07-11 20:27:57 +0200

    middlewares y utils (#27)
    Co-authored-by: jaengustavof <jaengustavof@gmail.com>

--- middlewares/checker.js
+++ middlewares/checker.js
@@ -1,7 +1,8 @@
-module.exports = (...fields) => (req, res, next) => {
-    for(const field of fields) {
-        if(!req.body[field]) return next("error en los campos") //pendiente pasar codig de error cuando est'en hechos
-    }
+// const { generic } = require("../errors/auth"); //en cuanto tengamos los errors
 
-    next()
-}
+module.exports = (...fields) => (req, res, next) =>{
+    for(const field of fields) {
+        if(!req.body[field]) return next(generic["empty"])
+    }
+    next();
+};
