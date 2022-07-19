ee07742cc5ec99e2adc3304bc613764f79721d3e
AlbertoArenas98 <92597288+AlbertoArenas98@users.noreply.github.com>
2022-07-19 21:25:48 +0200

    fix(register): service and queries (#54)
    * fix: deleted unused cors
    
    * fix: register service and queries
    
    * feat: register checker
    
    *fix: docker compose PORT and Variables
    
    Co-authored-by: Brujitari <natimontesg2@mail.com>
    Co-authored-by: sorayarg <sorayarg25@gmail.com>

--- errors/auth.js
+++ errors/auth.js
@@ -1,20 +1,23 @@
 module.exports = {
-    generic: {
-      empty: {
-        statusCode: 400,
-        error: new Error("all fields are mandatory"),
-      },
-    },
+
     login: {
       unknown: {
         statusCode: 400,
         error: new Error("user or password incorrect"),
+      },
+      empty: {
+        statusCode: 400,
+        error: new Error("all fields are mandatory"),
       },
     },
     register: {
       duplication: {
         statusCode: 400,
         error: new Error("user already exists"),
+      },
+      empty: {
+        statusCode: 400,
+        error: new Error("these fields are mandatory"),
       },
     },
   };
