ed51eb5c55a6ad3f48ae08522f2531099fe62fcf
sorayarg <sorayarg25@gmail.com>
2022-07-19 21:12:30 +0200

    feat: register checker

--- services/auth/register.js
+++ services/auth/register.js
@@ -1,5 +1,5 @@
 const { createUser } = require('../../queries/auth');
-const { hash, sendEmail } = require('../../utils');
+const { hash } = require('../../utils');
 const { register } = require('../../errors/auth');
 const errors = require('../../errors/commons');
 
@@ -18,15 +18,15 @@ module.exports = db => async (req, res, next) => {
 
   const message = 'Gracias por registrarte en mi Ecommerce, disfruta de nuestros productos';
 
-  try {
-    await sendEmail({
-      email,
-      subject: 'Bienvenido a mi Ecommerce, gracias por registrarte',
-      message,
-    });
-  } catch (error) {
-    console.log('> error: ', error.message);
-  }
+  // try {
+  //   await sendEmail({
+  //     email,
+  //     subject: 'Bienvenido a mi Ecommerce, gracias por registrarte',
+  //     message,
+  //   });
+  // } catch (error) {
+  //   console.log('> error: ', error.message);
+  // }
 
   res.status(200).json({
     success: true,
