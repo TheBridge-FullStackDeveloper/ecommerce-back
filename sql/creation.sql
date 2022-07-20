2f9f6066b9042ed1852ef49f85fdf9e5b1fc3bb9
jaengustavof <jaengustavof@gmail.com>
2022-07-20 14:09:44 +0200

    cambios

--- sql/creation.sql
+++ sql/creation.sql
@@ -52,17 +52,6 @@ CREATE TABLE IF NOT EXISTS sells (
   delivery_date DATE NOT NULL,
   receiving_date DATE NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS sells (
-  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  ref TEXT NOT NULL UNIQUE,
-  user_id uuid REFERENCES users
-    ON UPDATE CASCADE
-    ON DELETE CASCADE,
-  delivery_date DATE NOT NULL,
-  receiving_date DATE NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS orders (
   quantity INTEGER NOT NULL DEFAULT 1,
   sell_id uuid REFERENCES sells
