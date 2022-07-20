0c393f8c8317fcd7da2589aaa243c024ab752c1a
carlitos_madrigal <madrigalgallego.jc@gmail.com>
2022-07-20 19:46:50 +0200

    feat: login/queries

--- queries/auth/index.js
+++ queries/auth/index.js
@@ -27,8 +27,34 @@ const createUser =
     )(insertUser({ email, password, first_name, address, role }));
   };
 
+  const getCorrectUser = (db) => async ({ email, compareFn }) => {
+    const user = await getFullUser(db)({ email });
+
+    if (!user.data) {
+      return {
+        ok: false,
+        code: "unknown",
+      };
+    }
+
+    const isPasswordCorrect = await compareFn(user.data.password);
+
+    if (!isPasswordCorrect) {
+      return {
+        ok: false,
+        code: "unknown",
+      };
+    }
+
+    return {
+      ok: true,
+      data: { email: user.data.email },
+    };
+  };
+
 
   module.exports = {
     getFullUser,
     createUser,
+    getCorrectUser,
   };
--- services/auth/index.js
+++ services/auth/index.js
@@ -5,10 +5,11 @@ const forms = {
   register: ['email', 'password', 'first_name', 'role'],
   login: ['email', 'password']
 }
-const type = "register"
+const type = { register: "register", login: "login" }
+
 module.exports = (db) => {
-  router.post("/register", checker(type,...forms.register), require("./register")(db));
-  //router.post("/login", checker(...forms.login), require("./login")(db));
+  router.post("/register", checker(type.register,...forms.register), require("./register")(db));
+  router.post("/login", checker(type.login,...forms.login), require("./login")(db));
   //router.post("/logout", authorizer, require("./logout")());
 
   return router;
