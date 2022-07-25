3c93badfb0e5a3f2e82c06a7bccbe0e5a7f33863
jaengustavof <jaengustavof@gmail.com>
2022-07-25 15:18:42 +0200

    queries

---
+++ queries/orders/queries.js
@@ -0,0 +1,37 @@
+const { sql } = require("slonik");
+
+const selectOneOrder = ({ sell_id }) => {
+  return sql`
+        SELECT *
+        FROM orders
+        WHERE sell_id = ${sell_id};
+    `; // ver si dejamos sell_id o cambiamos a otra ref
+};
+
+const selectAllOrders = () => {
+  return sql`
+  SELECT * FROM orders;`;
+};
+
+const insertOneOrder = ({ quantity, sell_id, product_id }) => {
+ // console.log(quantity, sell_Id, product_id)
+  return sql` INSERT INTO orders (quantity, sell_id, product_id) 
+  VALUES (${quantity}, (SELECT id FROM sells WHERE id = ${sell_id}),
+  (SELECT id FROM products WHERE id = ${product_id}));
+  `;
+};
+
+const deleteOneOrder = ({ sell_id }) => {
+  console.log(sell_id )
+  return sql`
+      DELETE FROM orders
+      WHERE sell_id = ${sell_id}
+  `; // ver si dejamos sell_id o cambiamos a otra ref
+};
+
+module.exports = {
+  selectOneOrder,
+  selectAllOrders,
+  insertOneOrder,
+  deleteOneOrder,
+};
