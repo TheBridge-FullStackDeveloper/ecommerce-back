7160d340ceacf6af3e0c94643f2ae3be47d5531d
jaengustavof <jaengustavof@gmail.com>
2022-07-25 15:24:02 +0200

    test completed

--- services/orders/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-module.exports = (db) => {
-    router.get("/get-all", require("./get-all")(db));
-    router.post("/create", require("./create")(db));
-    router.post("/delete", require("./delete")(db));
-    
-    return router;
-}
