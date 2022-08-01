eeeb2cded62c16e2879a292d5040c04c4b20f332
Brujitari <natimontesg2@mail.com>
2022-08-01 20:43:17 +0200

    fix: authorizer missing

--- services/orders/index.js
+++ services/orders/index.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
+const { authorizer } = require("../../middlewares")
 
 module.exports = (db) => {
     router.get("/get-all", require("./get-all")(db));
     router.post("/create", require("./create")(db));
     router.post("/delete", require("./delete")(db));
-    router.post("/get-one", require("./get-one")(db))
+    router.post("/get-one", authorizer, require("./get-one")(db))
     
     return router;
 }
